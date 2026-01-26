829b5b278eb4d42c257c89e5c8c06faf32863908
lauralcodes <charish.weerasuriya@gmail.com>
2026-01-27 00:57:03 +0100

    initial commit

---
+++ src/main/java/se/jensen/charitha/meetspaceserver/MeetSpaceServerApplication.java
@@ -0,0 +1,13 @@
+package se.jensen.charitha.meetspaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetSpaceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetSpaceServerApplication.class, args);
+    }
+
+}
